380bdc6556707a1f6b0ec7310a39a01177d43cc4
Your Name <you@example.com>
2024-02-19 18:39:29 -0500

    Player area Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-
+import Player from "./components/Player"
 function App() {
   
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">   
+        <Player initialName ="Player 1" symbol="O"/>
+        <Player initialName ="Player 2" symbol="X"/>        
+        </ol>
+        GameBoard 
+
+      </div>
+
+
+      Log
+    </main>
+
   )
 }
 
